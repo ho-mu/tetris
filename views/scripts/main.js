3e4fb99cef15ac0aec80669572e6b97db86f4b92
Holly Murphy <holly.m.murphy@hotmail.com>
2017-05-18 16:48:05 -0500

    updates

--- views/scripts/main.js
+++ views/scripts/main.js
@@ -8,6 +8,10 @@ function drawBoard(){
 				html_str+=`<div class='blank'></div>`
 			}else if(board[i][j] == 1){
 				html_str+=`<div class='filled'></div>`
+			}else if(board[i][j] =='l'){
+				html_str+=`<div class='filled l'></div>`
+			}else if (board[i][j] == '99'){
+				html_str+=`<div class='off_board'></div>`
 			}
 		}//innerfor
 		html_str+=`</div>`
@@ -21,32 +25,31 @@ function generateShape(){//calling method will need to pass in the element
 	var html_shape = ''
 	//l[element].length will give you the height
 	//l[element][0].length will give you the number of columns of the shape
-	for(var x=0; x<l[0].length; x++){//# of rows
+	for(var x=0; x<shapes.line[0].length; x++){//# of rows
 		html_shape+=`<div class='shape'>`
-		for(var y=0; y<l[0][x].length; y++){
-			if(l[0][x][y]===0){
+		for(var y=0; y<shapes.line[0][x].length; y++){
+			if(shapes.line[0][x][y]===0){
 				html_shape += `<div class='blank_shape l'></div>`
-			}else if(l[0][x][y] ===1){
+			}else if(shapes.line[0][x][y] ===1){
 				html_shape += `<div class='filled_shape l'></div>`
 			}//end elseif
 		}
-}
-
-	//for(var x=0; x<l.length; x++){ //however many elements in the array
-		
-		// for(var y=0; y<l[0].length; y++){ //however many elements in the top row of the first array in the first element
-		// 	html_shape+=`<div class='shape'>`
-		// 	for(var z=0; z<l[0][0]; z++){
-		// 		if(l[0][y][z]===0){
-		// 			html_shape += `<div class='blank_shape l'></div>`
-		// 		}else if(l[0][y][z] ===1){
-		// 			html_shape += `<div class='filled_shape l'></div>`
-		// 		}//end elseif
-		// 	}//end innermost for
-		// }//end inner for
+	}
 		html_shape+=`</div>`
 	//}//end outer for
 	$('#shape').html(html_shape)
+}
+
+function moveShape(){
+	//update x=0y=5 with l
+	//on the 3rd pass though, the l
+	var row_placement = 1 //top
+
+	for(var s=0;s<board[row_placement-1].length; s++){
+		//in cell 3, add the left corner of the shape
+		board[row_placement-1][s+3]="l"
+	}
+	drawBoard()
 }
 
 $(document).ready(function(){
